7e7cde6035ef3633773827259e93c2cf073b1a86
Alionor <elegrisa32@libero.it>
2026-03-03 16:37:25 +0100

    Classe Negozio impostata

--- src/it/prova/centrocommerciale/model/Negozio.java
+++ src/it/prova/centrocommerciale/model/Negozio.java
@@ -1,4 +1,72 @@
 package it.prova.centrocommerciale.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Negozio {
+
+    String ragioneSociale;
+    String partitaIVA;
+    CentroCommerciale centroCommerciale;
+    List<Item> item = new ArrayList<>();
+
+    public Negozio(String ragioneSociale, String partitaIVA, CentroCommerciale centroCommerciale, List<Item> item) {
+        this.ragioneSociale = ragioneSociale;
+        this.partitaIVA = partitaIVA;
+        this.centroCommerciale = centroCommerciale;
+        this.item = item;
+    }
+
+    public Negozio() {
+    }
+
+    public String getRagioneSociale() {
+        return ragioneSociale;
+    }
+
+    public void setRagioneSociale(String ragioneSociale) {
+        this.ragioneSociale = ragioneSociale;
+    }
+
+    public String getPartitaIVA() {
+        return partitaIVA;
+    }
+
+    public void setPartitaIVA(String partitaIVA) {
+        this.partitaIVA = partitaIVA;
+    }
+
+    public CentroCommerciale getCentroCommerciale() {
+        return centroCommerciale;
+    }
+
+    public void setCentroCommerciale(CentroCommerciale centroCommerciale) {
+        this.centroCommerciale = centroCommerciale;
+    }
+
+    public List<Item> getItem() {
+        return item;
+    }
+
+    public void setItem(List<Item> item) {
+        this.item = item;
+    }
+
+    /*public boolean addToItems(Lavoratore lavoratore, Item item) {
+
+    }
+
+    public boolean removeFromItems(Lavoratore lavoratore, Item item) {
+
+    }*/
+
+    @Override
+    public String toString() {
+        return "Negozio{" +
+                "ragioneSociale='" + ragioneSociale + '\'' +
+                ", partitaIVA='" + partitaIVA + '\'' +
+                ", centroCommerciale=" + centroCommerciale +
+                ", item=" + item +
+                '}';
+    }
 }
